bfaee04d2759cc299099574b619d864d482b51f0
Lemonjai <leocheung_1986@hotmail.com>
2015-09-20 19:30:01 -0400

    dropbox pulled images on production

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,11 @@
 class Blog < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "/images/picturenotfound.jpg",
-  :storage => :dropbox,
-  :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	if Rails.env.development?
+		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "/images/picturenotfound.jpg"
+	else
+	    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "/images/picturenotfound.jpg",
+		  							    :storage => :dropbox,
+									    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+									    :path => ":style/:id_filename"
+									    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	end
 end
