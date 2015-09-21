d9a01da15cbb4c23495dd8f1b19eb00cabff256f
Lemonjai <leocheung_1986@hotmail.com>
2015-09-21 08:51:57 -0400

    fixed the wrong image display

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,9 @@
 class Blog < ActiveRecord::Base
 	if Rails.env.development?
-		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "/images/picturenotfound.jpg"
+		has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
 										validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	else
-	    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "/images/picturenotfound.jpg",
+	    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg",
 		  							    :storage => :dropbox,
 									    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 									    :path => ":style/:id_filename"
